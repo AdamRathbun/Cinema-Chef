44364897a5f9bae40d6420030ea900c548a11ac8
AdamRathbun <hesle1119@gmail.com>
2024-03-23 22:50:56 -0400

    minor

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,34 +1,32 @@
 import React from 'react';
-import './Navbar.css';
 import { Link } from 'react-router-dom';
 import AuthComponent from '../Auth/Auth';
 import SearchBar from '../SearchBar/SearchBar';
+import './Navbar.scss'
 
 function Navbar({ userId }) {
 
   return (
     <nav className='navbar'>
-      <ul className='navbar-nav'>
-        <li className='navbar-item'>
-          <Link to="/">Home</Link>
+      <ul className='navbar__list'>
+        <li className='navbar__item'>
+          <Link to="/" className='navbar__link'>Home</Link>
         </li>
-        <li className='navbar-item'>
-          <Link to="/user-recipes">Your recipes</Link>
+        <li className='navbar__item'>
+          <Link to="/user-recipes" className='navbar__link'>Your recipes</Link>
         </li>
-        <li className='navbar-item'>
-          <Link to="/add-recipe">Add a recipe</Link>
+        <li className='navbar__item'>
+          <Link to="/add-recipe" className='navbar__link'>Add a recipe</Link>
         </li>
         {userId && (
-          <li className='navbar-item'>
-            <Link to={`/saved-recipes`}>Saved recipes</Link>
+          <li className='navbar__item'>
+            <Link to={`/saved-recipes`} className='navbar__link'>Saved recipes</Link>
           </li>
         )}
-        <li>
+        <div className='navbar--right'>
           <AuthComponent />
-        </li>
-        <li>
           <SearchBar />
-        </li>
+        </div>
       </ul>
     </nav>
   );
